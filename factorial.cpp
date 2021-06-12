6baaf90a7ff0211faad4d89ff106fbf8a584658f
Narek <narek.ikhtiaryan.ni@gmail.com>
2021-06-12 11:56:30 -0700

    add factorial.h and test.cc files and made some changes in code for testing

--- factorial.cpp
+++ factorial.cpp
@@ -1,66 +1,32 @@
-#include <iostream>
-#include <string>
-#include <exception>
-
-using std::cout;
-using std::cin;
-using std::endl;
-using std::string;
-using std::stoi;
-using std::exception;
-
-
-class myexception: public exception {
-    public:
-	virtual const char* what() const throw() {
-	    return "\nError: a negative or very large number:";
-	}
-} myex;
-
+#include "factorial.h"
 
 
 //check if number or string
 void check_number(string str) {
     for (int i = 0; i < str.length(); i++){
-	if (isdigit(str[i]) == false){
-	    throw 0;
-	}
+        if (isdigit(str[i]) == false){
+            throw 0;
+        }
     }
+}
+
+
+// function overloading for string
+long long int factorial(string n){
+   return -1;
 }
 
 
 // calculate factorial
 unsigned long long int factorial(int n){
-    unsigned long long int result = 1;
+  if(n < 0){
+    return -1;
+  }
+
+  unsigned long long int result = 1;
     for (int i = 2; i <= n; i++){
-	result *= i;
+      result *= i;
     }
-    return result;
-}
-
-
-int main(){
-    try{
-	string number_str;
-	cout << "Enter number: ";
-	cin >> number_str;
-	
-	check_number(number_str);
-	
-	int number_int = stoi(number_str); 
-	//std::stoi function returns the converted integral number as an int value (base 10).
-
-	if (number_int < 0 || number_int >= 66){
-	    throw myex;
-	}else{
-	    cout << number_int << "! = " << factorial(number_int) << endl;
-	}
-    }
-    catch(const exception& e){
-	cout << e.what() << '\n';
-    }
-    catch(const int& e){
-	cout << "\nThe input isn't int!" << '\n';
-    }
+  return result;
 }
 
